a0026f4905558a8debf9ac1f12e0906520dc9a5c
amanaggarwal1 <i.aggarwalaman@gmail.com>
2020-03-22 15:46:28 +0530

    Game is now in a playable state, now it alternatively switches between players after they choose any position on board

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.Constraints;
 import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,36 +14,56 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GridLayout boardGL;
+    // Declaring views
     private TextView information;
     private Button clearButton;
-    private ImageView position1, position2, position3, position4, position5, position6, position7, position8, position9;
 
+    private boolean activePlayer = false;
+    // activePlayer = false means that yellow is the active player
+    // activePlayer = true means that red is the active player
+
+    int[] gameStatus = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    //value 2 at any index refers that it is empty, i.e not occupied by any player
+    //value 0 at any index refers that it is occupied by yellow
+    //value 1 at any index refers that it is occupied by red
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boardGL = findViewById(R.id.boardGridLayout);
+        //Linking views
         information = findViewById(R.id.informationTV);
         clearButton = findViewById(R.id.clearButton);
-        position1 = findViewById(R.id.position1IV);
-        position2 = findViewById(R.id.position2IV);
-        position3 = findViewById(R.id.position3IV);
-        position4 = findViewById(R.id.position4IV);
-        position5 = findViewById(R.id.position5IV);
-        position6 = findViewById(R.id.position6IV);
-        position7 = findViewById(R.id.position7IV);
-        position8 = findViewById(R.id.position8IV);
-        position9 = findViewById(R.id.position9IV);
-
     }
 
-    public void dropIn(View view){
-        ImageView v = (ImageView) view;
-        v.setTranslationY(-1000);
-        v.setImageResource(R.drawable.yellow);
-        v.animate().translationYBy(1000).rotationBy(1800).setDuration(300);
+    //dropIn function gets called when any of the grid layout position is tapped
+    public void dropIn(View v){
+        ImageView image = (ImageView) v;
+        final String updatedInfo;
+
+        image.setTranslationY(-1000); // Initially translating the image beyond the screen in upward direction
+
+        //Setting image resource and information text view depending on active Player
+        if(activePlayer) {
+            image.setImageResource(R.drawable.red);
+            updatedInfo = "Yellow's Turn";
+        } else{
+            image.setImageResource(R.drawable.yellow);
+            updatedInfo = "Red's Turn";
+        }
+
+        //information text update takes a delay of 300ms
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                information.setText(updatedInfo);
+            }
+        }, 300);
+
+        activePlayer ^= true; //Changing the value of active player
+
+        image.animate().translationYBy(1000).rotationBy(1800).setDuration(300);
     }
 }
